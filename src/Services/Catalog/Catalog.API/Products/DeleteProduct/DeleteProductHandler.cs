d5b488d0e887234227361de5e4312e0b253f5aeb
anhphan237 <anh713988@gmail.com>
2025-02-11 18:04:23 +0700

    Develop DeleteProduct DELETE Endpoint with Minimal Apis and Carter

--- src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -1,27 +1,29 @@
 ﻿
 
-namespace Catalog.API.Products.DeleteProduct;
-
-public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
-public record DeleteProductResult(bool IsSuccess);
-
-public class DeleteProductCommandValidator : AbstractValidator<DeleteProductCommand>
+namespace Catalog.API.Products.DeleteProduct
 {
-    public DeleteProductCommandValidator()
+    public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+    public record DeleteProductResult(bool IsSuccess);
+
+    public class DeleteProductCommandValidator : AbstractValidator<DeleteProductCommand>
     {
-        RuleFor(x => x.Id).NotEmpty().WithMessage("Product ID is required");
+        public DeleteProductCommandValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Product ID is required");
+        }
+    }
+
+    internal class DeleteProductCommandHandler
+        (IDocumentSession session)
+        : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+    {
+        public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+        {
+            session.Delete<Product>(command.Id);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new DeleteProductResult(true);
+        }
     }
 }
 
-internal class DeleteProductCommandHandler
-    (IDocumentSession session)
-    : ICommandHandler<DeleteProductCommand, DeleteProductResult>
-{
-    public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
-    {
-        session.Delete<Product>(command.Id);
-        await session.SaveChangesAsync(cancellationToken);
-
-        return new DeleteProductResult(true);
-    }
-}
