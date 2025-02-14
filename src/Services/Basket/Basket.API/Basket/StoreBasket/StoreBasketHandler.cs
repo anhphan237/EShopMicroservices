e2fdf9d8679d26d5cdf5cf9946c06c3109204b36
anhphan237 <anh713988@gmail.com>
2025-02-14 23:38:06 +0700

    Develop StoreBasket Feature Handler class with CQRS and MediatR

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,6 +1,37 @@
-﻿namespace Basket.API.Basket.StoreBasket
+﻿
+
+namespace Basket.API.Basket.StoreBasket
 {
-    public class StoreBasketHandler
+    public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+    public record StoreBasketResult(string UserName);
+
+    public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
     {
+        public StoreBasketCommandValidator()
+        {
+            RuleFor(x => x.Cart).NotNull().WithMessage("Cart can not be null");
+            RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName is required");
+        }
+    }
+
+    public class StoreBasketCommandHandler
+        : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+    {
+        public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+        {
+            ShoppingCart cart = command.Cart;
+
+            return new StoreBasketResult("swn");
+        }
+
+        /*private async Task DeductDiscount(ShoppingCart cart, CancellationToken cancellationToken)
+        {
+            // Communicate with Discount.Grpc and calculate lastest prices of products into sc
+            foreach (var item in cart.Items)
+            {
+                var coupon = await discountProto.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken: cancellationToken);
+                item.Price -= coupon.Amount;
+            }
+        }*/
     }
 }
